3da1fce6a2ace730f1f651563c30dd88223eae69
blingbling-110 <523497359@qq.com>
2022-03-10 08:02:08 +0800

    feat: JS类型

---
+++ JS/类型/types.js
@@ -0,0 +1,12 @@
+console.log(`
+typeof undefined: ${typeof undefined}
+typeof null: ${typeof null}
+typeof false: ${typeof false}
+typeof 0: ${typeof 0}
+typeof 0n: ${typeof 0n}
+typeof '': ${typeof ''}
+typeof Symbol(): ${typeof Symbol()}
+typeof {}: ${typeof {}}
+typeof console.log: ${typeof console.log}
+[] == ![]: ${[] == ![]}
+`)
