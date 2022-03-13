baf562fe84d501e41b643812671274440ba7dde9
blingbling-110 <523497359@qq.com>
2022-03-14 03:41:14 +0800

    feat: 自定义Promise

---
+++ JS/Promise/index.js
@@ -0,0 +1,191 @@
+// 状态常量
+const PENDING = 'pending'
+const RESOLVED = 'resolved'
+const REJECTED = 'rejected'
+
+// 自定义Promise
+class MyPromise {
+    // 实例的状态
+    state = PENDING
+    // 实例的结果
+    result
+    // 解决回调队列
+    resolveQueue = []
+    // 拒绝回调队列
+    rejectQueue = []
+
+    // 接收执行器(resolve, reject) => {}
+    constructor(fn) {
+        // 需要捕获错误
+        try {
+            fn(this.resolve, this.reject)
+        } catch (reason) {
+            // 发生错误则拒绝
+            this.reject(reason)
+        }
+    }
+
+    // 解决函数
+    resolve = value => {
+        if (value instanceof MyPromise) {
+            // 若嵌套Promise则递归解决
+            return value.then(this.resolve, this.reject)
+        }
+        if (this.state !== PENDING) {
+            return
+        }
+        // 异步执行，否则回调可能还没入列Promise就已经落定了
+        setTimeout(() => {
+            this.state = RESOLVED
+            this.result = value
+            this.resolveQueue.forEach(cb => cb())
+        })
+    }
+
+    // 拒绝函数
+    reject = reason => {
+        if (this.state !== PENDING) {
+            return
+        }
+        setTimeout(() => {
+            this.state = REJECTED
+            this.result = reason
+            this.rejectQueue.forEach(cb => cb())
+        })
+    }
+
+    // 排期函数
+    then(onResolved, onRejected) {
+        // 必须返回新的Promise实例
+        let newPromise
+        switch (this.state) {
+            case PENDING:
+                newPromise = new MyPromise((resolve, reject) => {
+                    this.resolveQueue.push(() => {
+                        try {
+                            // 解决回调和拒绝回调均为可选参数，若不是函数则透传
+                            if (typeof onResolved !== 'function') {
+                                resolve(this.result)
+                            } else {
+                                this.handleRes(onResolved(this.result), newPromise)
+                            }
+                        } catch (reason) {
+                            reject(reason)
+                        }
+                    })
+                    this.rejectQueue.push(() => {
+                        try {
+                            if (typeof onRejected !== 'function') {
+                                reject(this.result)
+                            } else {
+                                this.handleRes(onRejected(this.result), newPromise)
+                            }
+                        } catch (reason) {
+                            reject(reason)
+                        }
+                    })
+                })
+                break;
+            case RESOLVED:
+                // 异步执行排期回调
+                newPromise = new MyPromise((resolve, reject) => setTimeout(() => {
+                    try {
+                        if (typeof onResolved !== 'function') {
+                            resolve(this.result)
+                        } else {
+                            this.handleRes(onResolved(this.result), newPromise)
+                        }
+                    } catch (reason) {
+                        reject(reason)
+                    }
+                }))
+                break;
+            case REJECTED:
+                newPromise = new MyPromise((resolve, reject) => setTimeout(() => {
+                    try {
+                        if (typeof onRejected !== 'function') {
+                            reject(this.result)
+                        } else {
+                            this.handleRes(onRejected(this.result), newPromise)
+                        }
+                    } catch (reason) {
+                        reject(reason)
+                    }
+                }))
+                break;
+            default:
+                break;
+        }
+        return newPromise
+    }
+
+    // 处理排期回调结果
+    handleRes(newResult, newPromise) {
+        if (newResult === newPromise) {
+            // 若回调执行结果和then()需要返回的Promise实例相同，则出现循环引用
+            return newPromise.reject(new TypeError('There is a reference circle'))
+        }
+        if (newResult instanceof MyPromise) {
+            // 回调执行结果可能为Promise实例
+            if (newResult.state === PENDING) {
+                // 若状态为待定则继续排期，直到某个Promise实例落定
+                newResult.then(value => this.handleRes(value, newPromise), newPromise.reject)
+            } else {
+                // 若状态为落定则将newPromise落定
+                newResult.then(newPromise.resolve, newPromise.reject)
+            }
+            return
+        }
+        if (newResult !== null && (typeof newResult === 'object' || typeof newResult === 'function')) {
+            // 若排期结果为对象或函数，则调用thenable接口，若未实现thenable接口则直接解决
+            // 确保在result不为Promise实例但实现thenable接口时只执行一次resolve或reject（模拟Promise状态机）
+            let called = false
+            try {
+                let then = newResult.then
+                if (typeof then === 'function') {
+                    // 调用thenable接口
+                    then.call(newResult, value => {
+                        if (called) {
+                            return
+                        } else {
+                            called = true
+                            this.handleRes(value, newPromise)
+                        }
+                    }, reason => {
+                        if (called) {
+                            return
+                        } else {
+                            called = true
+                            newPromise.reject(reason)
+                        }
+                    })
+                } else {
+                    // 未实现thenable接口，直接解决
+                    newPromise.resolve(newResult)
+                }
+            } catch (reason) {
+                if (called) {
+                    return
+                } else {
+                    called = true
+                    newPromise.reject(reason)
+                }
+            }
+        } else {
+            // 若排期结果为基本类型，则直接解决
+            newPromise.resolve(newResult)
+        }
+    }
+
+    // promises-aplus-tests
+    static deferred() {
+        const res = {}
+        res.promise = new MyPromise((resolve, reject) => {
+            res.resolve = resolve
+            res.reject = reject
+        })
+        return res
+    }
+}
+
+module.exports = MyPromise
