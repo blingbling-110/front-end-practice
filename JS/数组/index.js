ef06bba90de8270dc989ad3d2b5481be1d4b726b
qinzijun <qinzijun@hongwuniu.com>
2022-03-14 20:12:04 +0800

    feat: map(), flat(), 自定义flat

---
+++ JS/数组/index.js
@@ -0,0 +1,55 @@
+/*
+var new_array = arr.map(function callback(currentValue[, index[, array]]) {
+ // Return element for new_array 
+}[, thisArg])
+*/
+console.log(`
+------------------ map() ------------------
+['1', '2', '3'].map(parseInt): ${['1', '2', '3'].map(parseInt)}
+`)
+/*
+parseInt(string, radix);
+如果 radix 是 undefined、0或未指定的，JavaScript会假定以下情况：
+1.如果输入的 string以 "0x"或 "0x"（一个0，后面是小写或大写的X）开头，
+    那么radix被假定为16，字符串的其余部分被当做十六进制数去解析。
+2.如果输入的 string以 "0"（0）开头， radix被假定为8（八进制）或10（十进制）。
+    具体选择哪一个radix取决于实现。ECMAScript 5 澄清了应该使用 10 (十进制)，
+    但不是所有的浏览器都支持。因此，在使用 parseInt 时，一定要指定一个 radix。
+3.如果输入的 string 以任何其他值开头， radix 是 10 (十进制)。
+*/
+
+/*
+var newArray = arr.flat([depth])
+*/
+console.log(`
+------------------ flat() ------------------
+[1, 2, , 4, 5]: ${[1, 2, , 4, 5]}
+[1, 2, , 4, 5].flat(): ${[1, 2, , 4, 5].flat()}
+[1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]].flat(Infinity):
+`);
+const arr = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]]
+console.log(arr.flat(Infinity))
+// 自定义flat
+Array.prototype.myFlat = function (depth = 1) {
+    if (depth > 0) {
+        return this.reduce((acc, val) => acc.concat(Array.isArray(val) ? val.myFlat(depth - 1) : val), [])
+    } else {
+        return this.slice()
+    }
+}
+// 使用生成器实现flat
+function* gFlat(arr, depth = 1) {
+    if (depth > -1) {
+        for (const item of arr) {
+            if (Array.isArray(item)) {
+                yield* gFlat(item, depth - 1)
+            } else {
+                yield item
+            }
+        }
+    } else {
+        yield arr.slice()
+    }
+}
+console.log(arr.myFlat())
+console.log([...gFlat(arr, 2)])
