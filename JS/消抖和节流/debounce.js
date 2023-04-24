432428aad05a0ae8247b8d2c43698ae55f0c71da
blingbling-110 <523497359@qq.com>
2023-04-24 10:22:45 +0800

    fix: review

--- JS/消抖和节流/debounce.js
+++ JS/消抖和节流/debounce.js
@@ -1,38 +1,15 @@
-function debounce(func, delay = 300, imm = true) {
-    let begin, timer, ctx, args, res
-    const cb = () => {
-        // 注意此处的begin为最后一次执行返回函数的时间戳
-        // 因此，delta是计时器结束时距最后一次执行返回函数的时间差
-        const delta = new Date().getTime() - begin
-        if (delta < delay) {
-            // 执行返回函数的时间间隔还不够，需要将剩余时间也耗尽
-            timer = setTimeout(cb, delay - delta)
-        } else {
-            // 解引用避免内存泄漏
-            timer = null
-            if (!imm) {
-                res = func.apply(ctx, args)
-                // 解引用避免内存泄漏
-                !timer && (ctx = args = null)
-            }
-        }
-    }
+function debounce(func, delay = 300, imm = false) {
+    let timer = null
     return function () {
-        ctx = this
-        args = arguments
-        begin = new Date().getTime()
-        if (!timer) {
-            timer = setTimeout(cb, delay)
-            if (imm) {
-                // 注意执行目标函数需要做到：
-                // 1.绑定上下文
-                // 2.传递入参
-                // 3.返回结果
-                res = func.apply(ctx, args)
-                // 解引用避免内存泄漏
-                ctx = args = null
-            }
+        // 定时器还未执行则清除
+        timer && clearTimeout(timer)
+        if (!timer && imm) {
+            func.apply(this, arguments)
         }
-        return res
+        timer = setTimeout(() => {
+            !imm && func.apply(this, arguments)
+            // 将timer解引用，表明定时器已经执行
+            timer = null
+        }, delay)
     }
 }
--- JS/消抖和节流/throttle.js
+++ JS/消抖和节流/throttle.js
@@ -1,45 +1,15 @@
 function throttle(func, delay = 300, opt = {}) {
-    let timer, ctx, args, res
-    let timestamp = 0
-    const cb = () => {
-        // 解引用避免内存泄漏
-        timer = null
-        // 记录本次执行目标函数的时间戳
-        timestamp = new Date().getTime()
-        res = func.apply(ctx, args)
-        // 解引用避免内存泄漏
-        !timer && (ctx = args = null)
-    }
+    const leading = opt?.leading ?? false,
+        trailing = opt?.trailing ?? true
+    let timer = null
     return function () {
-        ctx = this
-        args = arguments
-        const now = new Date().getTime()
-        if (opt.leading === false) {
-            // 若不需要在节流开始时执行目标函数，则此处需要设置时间戳来使delta为0
-            timestamp = now
-        }
-        // 若需要在节流开始执行目标函数，则delta就是距离最后一次执行目标函数的时间差
-        const delta = now - timestamp
-        if (delta > delay) {
-            // 时间差大于节流时间也需要判断计时器，因为setTimeout的执行时间可能比设置值更晚
-            if (timer) {
-                clearTimeout(timer)
-                // 解引用避免内存泄漏
-                timer = null
-            }
-            // 记录本次执行目标函数的时间戳
-            timestamp = now
-            // 注意执行目标函数需要做到：
-            // 1.绑定上下文
-            // 2.传递入参
-            // 3.返回结果
-            res = func.apply(ctx, args)
-            // 解引用避免内存泄漏
-            !timer && (ctx = args = null)
-        } else if (!timer && opt.trailing !== false) {
-            // 若时间差小于节流时间，并且此时已经没有计时器了，同时还需要在节流结束时执行目标函数
-            timer = setTimeout(cb, delay - delta)
-        }
-        return res
+        // 定时器还未执行则直接返回
+        if (timer) return
+        leading && func.apply(this, arguments)
+        timer = setTimeout(() => {
+            trailing && func.apply(this, arguments)
+            // 将timer解引用，表明定时器已经执行
+            timer = null
+        }, delay)
     }
 }
